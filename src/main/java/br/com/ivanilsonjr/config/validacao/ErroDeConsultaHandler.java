eb6293e01689e7bedf73f46fc7eab4e0158a949c
Ivanilson Junior <ivanilsonaraujojr@gmail.com>
2020-10-07 10:41:33 -0300

    Adicionando tratamento de exception geral do resttemplate

--- src/main/java/br/com/ivanilsonjr/config/validacao/ErroDeConsultaHandler.java
+++ src/main/java/br/com/ivanilsonjr/config/validacao/ErroDeConsultaHandler.java
@@ -8,14 +8,24 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.servlet.ModelAndView;
 
 
 @ControllerAdvice
 public class ErroDeConsultaHandler{
 
+	@ExceptionHandler(value=RestClientException.class)
+	@ResponseStatus(code=HttpStatus.INTERNAL_SERVER_ERROR)
+	public ModelAndView handle(RestClientException exception,Model model){
+			ModelAndView mv = new ModelAndView("error/erroView");
+			mv.addObject("mensagemCodigo", 500);
+			mv.addObject("mensagemErro","Erro desconhecido, verifique com o administrador.");
+			return mv;
+	}
+
 	@ExceptionHandler(value=HttpClientErrorException.class)
-	@ResponseStatus(code=HttpStatus.NOT_FOUND)
+	@ResponseStatus(code=HttpStatus.BAD_REQUEST)
 	public ModelAndView handle(HttpClientErrorException exception,Model model){
 			ErroDto erro = new ErroDto(exception.getRawStatusCode(), exception.getStatusText());
 			ModelAndView mv = new ModelAndView("error/erroView");
