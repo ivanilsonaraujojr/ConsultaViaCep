7ad265eacb3477c56a85bc4b0faf4b922a9d3fa0
Ivanilson Junior <ivanilsonaraujojr@gmail.com>
2020-10-07 10:11:17 -0300

    Refatorando serviço de consulta a api do VIACEP

--- src/main/java/br/com/ivanilsonjr/service/impl/ViaCepServiceImpl.java
+++ src/main/java/br/com/ivanilsonjr/service/impl/ViaCepServiceImpl.java
@@ -2,10 +2,6 @@ package br.com.ivanilsonjr.service.impl;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -15,28 +11,30 @@ import br.com.ivanilsonjr.model.Consulta;
 import br.com.ivanilsonjr.service.ViaCepService;
 
 @Service
-public class ViaCepServiceImpl implements ViaCepService{
+public class ViaCepServiceImpl implements ViaCepService {
 
 	@Bean
 	public RestTemplate restTemplate() {
-	    return new RestTemplate();
+		return new RestTemplate();
 	}
-	
+
 	@Value("${viacepapi.url}/")
 	private String urlWsViaCep;
 
 	@Override
-	public ResponseEntity<ConsultaDto> consultarCep(String cep){
-		String urlRequest = urlWsViaCep + cep + "/json";
-		HttpHeaders headers = new HttpHeaders();
-		headers.set("Accept", "application/json");
-		HttpEntity<Consulta> entity = new HttpEntity<Consulta>(headers);
-		ResponseEntity<Consulta> responseStatus = restTemplate().exchange(urlRequest, HttpMethod.GET, entity, Consulta.class);
-		
-		if(responseStatus.getBody().isErro() || responseStatus.getStatusCode() != HttpStatus.OK) {
+	public ResponseEntity<ConsultaDto> consultarCep(String cep) {
+			String urlRequest = urlWsViaCep + cep + "/json";
+		try {
+			ResponseEntity<Consulta> responseStatus = restTemplate().getForEntity(urlRequest, Consulta.class);
+
+			if (responseStatus.getBody().isErro() || !responseStatus.getStatusCode().is2xxSuccessful()) {
+				return null;
+			}
+			return ResponseEntity.ok(new ConsultaDto(responseStatus.getBody()));
+		} catch (Exception e) {
 			return null;
 		}
-		return ResponseEntity.ok(new ConsultaDto(responseStatus.getBody()));
-		}
-	
+
 	}
+
+}
